1ac5519ab25bde46cf7cf61a91960c036629a716
Alan Nava <alannavaseg_02@hotmail.com>
2020-02-27 14:44:31 -0600

    SE crean todos los botones y las operaciones basucas

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Text
+from tkinter import Tk, Text, Button, END, re
 class Interfaz:
     def __init__(self, ventana):
         #Iniciar la ventana con un título
@@ -15,6 +15,71 @@ class Interfaz:
         #Inicializar la operacion mostrada en pantalla como string vacío
         self.operacion  = ""
 
+        #Crear los botones de la calculadora
+        boton1 = self.crearBoton(7)
+        boton2 = self.crearBoton(8)
+        boton3 = self.crearBoton(9)
+        boton4 = self.crearBoton(u"\u232B", escribir = False)
+        boton5 = self.crearBoton(4)
+        boton6 = self.crearBoton(5)
+        boton7 = self.crearBoton(6)
+        boton8 = self.crearBoton(u"\u00F7")
+        boton9 = self.crearBoton(1)
+        boton10 = self.crearBoton(2)
+        boton11 = self.crearBoton(3)
+        boton12 = self.crearBoton("*")
+        boton13 = self.crearBoton(".")
+        boton14 = self.crearBoton(0)
+        boton15 = self.crearBoton("+")
+        boton16 = self.crearBoton("-")
+        boton17 = self.crearBoton("=", escribir = False, ancho = 20, alto = 2)
+        
+        #Ubicar los botones con el gestor grid
+        botones = [boton1, boton2,boton3, boton4, boton5, boton6, boton7, boton8, boton9, boton10,
+        boton11, boton12 , boton13, boton14, boton15, boton16, boton17]
+        contador = 0
+        for fila in range(1,5):
+            for columna in  range (4):
+                botones[contador].grid(row = fila, column = columna)
+                contador+= 1
+        #UBicar e ultimo boton al final
+        botones[16].grid(row = 5, column = 0, columnspan = 4)
+        return
+
+    def crearBoton(self, valor, escribir = True, ancho = 9, alto = 1):
+        return Button(self.ventana, text = valor, width = ancho, height = alto, font =("Helvetica", 15),
+        command = lambda:self.click(valor, escribir))
+    #Controla el evento disparado al hacer click en un boton
+    def click(self, texto, escribir):
+        #Si el parámetro 'escribir' es True, entonces el parámetro texto debe mostrarse en pantalla,
+        #Si es Falso, no
+        if not escribir:
+            if texto == "=" and self.operacion != "":
+                #Reemplazar el valor inocode de la division por el operador divisiion de Python '/'
+                self.operacion = re.sub(u"\u00F7","/", self.operacion)
+                resultado = str(eval(self.operacion))
+                self.operacion= ""
+                self.limpiarPantalla()
+                self.mostrarEnPantalla(resultado)
+            #Si se presionó el lbotón de borrado, limpiar la pantalla
+            elif texto == u"\u232B":
+                self.operacion  = ""
+                self.limpiarPantalla()
+        #Mostrar texto
+        else:
+            self.operacion += str(texto)
+            self.mostrarEnPantalla(texto)
+        return
+    #BOrra el contenido de la pantalla de la calculadora
+    def limpiarPantalla(self):
+        self.pantalla.configure(state= "normal")
+        self.pantalla.delete("1.0", END)
+        self.pantalla.configure(state="disabled")
+    #Muestra en la pantalla de la calculadora el contenido de las operaciones y los resultados
+    def mostrarEnPantalla(self, valor):
+        self.pantalla.configure(state= "normal")
+        self.pantalla.insert(END, valor)
+        self.pantalla.configure(state="disabled")
 ventana_principal = Tk()
 calculadora = Interfaz(ventana_principal)
 ventana_principal.mainloop()
